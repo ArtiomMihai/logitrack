e2642b4f6256a360f56c3a054d1c081385e5d36b
NikitaYatsko <archon04@mail.ru>
2025-11-30 04:18:46 +0200

    comments clear

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,34 +2,35 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function Header({ toggleMenu }) {
-
-
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={toggleMenu}>
-        <AntDesign name="align-left" size={36} color="black" />
-      </TouchableOpacity>
-      <View>
-        <Text style={styles.text}>LogiTrack</Text>
-      </View>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity onPress={toggleMenu} style={styles.menuButton}>
+                <AntDesign name="align-left" size={36} color="white" />
+            </TouchableOpacity>
+            <Text style={styles.text}>LogiTrack</Text>
+            <View style={{ width: 36 }} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    paddingLeft: 25,
-    paddingRight: 25,
-    marginTop: 34,
-    width: 100 + '%',
-    height: 88,
-    backgroundColor: "#A7A7A7",
-    flexDirection: "row",
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  text: {
-    color: 'black',
-    fontSize: 24,
-  },
+    container: {
+        paddingHorizontal: 20,
+        paddingTop: 25,
+        height: 80,
+        width: "100%",
+        backgroundColor: "#b94a48",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+
+    },
+    text: {
+        color: "#FFF",
+        fontSize: 24,
+        fontWeight: "700",
+    },
+    menuButton: {
+        padding: 5,
+    },
 });
