2990bcad6aef18f5377bfe86ac31ec49c03d9b1e
Artiom Mihai <mihaiartiom1706@gmail.com>
2025-12-02 00:50:06 +0200

    add order list page

---
+++ api.js
@@ -0,0 +1,7 @@
+export const API_BASE_URL = "https://juristic-zain-unconvened.ngrok-free.dev";
+
+export const ENDPOINTS = {
+    REGISTER: `${API_BASE_URL}/auth/register`,
+    LOGIN: `${API_BASE_URL}/auth/login`,
+    PROFILE: `${API_BASE_URL}/auth/me`,
+};
